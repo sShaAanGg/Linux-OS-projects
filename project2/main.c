765b00da800570aa404edc6b3f1c579b0c4294b1
sShaAanGg <sshaaangg.tw@gmail.com>
2022-12-01 14:12:56 +0800

    project2: modify main.c and readme
    Add system calls `fork()` and `waitpid()` in main.c "to check how memory
    areas are shared by two processes that execute this program
    simultaneously", which is the requirement of project2.

--- project2/main.c
+++ project2/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define __NR_get_task_mm 334
 #define __NR_get_phys_addr 335
@@ -23,6 +27,29 @@ void get_phys_addr_syscall(unsigned long virt_addr, unsigned long *addr_ptr)
 
 int main()
 {
+    pid_t child_pid, w;
+    int wait_status;
+    
+    child_pid = fork();
+    if (child_pid == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_pid == 0) {
+        printf("Child process PID is %d\n", (int) getpid());
+        sleep(2);
+        long l = get_task_mm_syscall();
+        printf("System call returned %ld\n", l);
+    } else {
+        printf("Parent process PID is %d\n", (int) getpid());
+        sleep(1);
+        long l = get_task_mm_syscall();
+        printf("System call returned %ld\n", l);
+        do {
+            w = waitpid(child_pid, &wait_status, WUNTRACED);
+
+        } while (!WIFEXITED(wait_status) && !WIFSIGNALED(wait_status));
+    }
 
     return 0;
 }
--- project2/readme.md
+++ project2/readme.md
@@ -5,4 +5,8 @@ Linux kernel version: 4.15.1
 Distribution: Ubuntu 16.04.7
 gcc: 5.4.0 
 
+### References
+> [fork(2) — Linux manual page](https://man7.org/linux/man-pages/man2/fork.2.html)  
+> [wait(2) — Linux manual page](https://man7.org/linux/man-pages/man2/wait.2.html)  
+
 #### 
